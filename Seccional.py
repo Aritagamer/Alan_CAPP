366c9dda3efc19fca1af00315c0233c92db78b7a
aritagamer <arita1817@gmail.com>
2021-03-01 23:05:31 -0600

    Finally

--- Seccional.py
+++ Seccional.py
@@ -30,7 +30,11 @@ class seccional:
       f = open(r'./SQL.txt','r+',encoding="utf-8")
       Tx = f.read()
       self.__Curs__.executescript(Tx)
+
+  def new_secc(self):
     
+    os.system("cls")
+
     print("Introduzca la seccion: ")
     self.Secc = input()
     
@@ -39,6 +43,13 @@ class seccional:
 
     print("Está cumpliendo el secciónal? ")
     self. Work = input()
+
+    if self.Work == "1":
+
+      print("Numero de registros que tiene: ")
+      self.Reg = input()
+    else:
+      self.Reg = 0
     
     print("Tiene grupo de WhatsApp? ")
     self.Whats = input()
@@ -49,9 +60,32 @@ class seccional:
     else:
       self.Integ = 0
 
-    self.__search__()
+  def old_secc(self):
 
+    os.system("cls")
 
+    print("Está cumpliendo el secciónal? ")
+    self. Work = input()
+
+    if self.Work == "1":
+
+      print("Numero de registros que tiene: ")
+      self.Reg = input()
+    else:
+      self.Reg = 0
+    
+    print("Tiene grupo de WhatsApp? ")
+    self.Whats = input()
+
+    if self.Whats == "1":
+      print("Número de integrantes en el grupo: ")
+      self.Integ = input()
+    else:
+      self.Integ = 0
+
+  def up_to_date(self , id):
+
+    self.__Curs__.executescript("UPDATE Registro set Labora = %d , Num_Registros = Num_Registros + %d , Grupo_Whats = %d , Num_Integrantes = Num_Integrantes + %d where id = %d" %(int(self.Work), int(self.Reg),int (self.Whats), int(self.Integ) , int(id)))
 
   def __search__(self):
 
@@ -59,19 +93,67 @@ class seccional:
       self.__Curs__.execute('SELECT * FROM Iztacalco WHERE S_E_%s = "%s"'%(i+1,self.Secc))
       Tables = self.__Curs__.fetchall()
 
-      if Tables:
+      self.__Curs__.execute('SELECT id FROM Registro')
+      Tables2 = self.__Curs__.fetchall()
 
+      cor = False
+
+      if Tables:
+        cor = True
         Tables = Tables[0]
         self.Zona = Tables[0]
         self.Region = Tables[1]
         self.Colonia = Tables[2]
 
         break
-    
-    
+        
+    if not cor:
 
-    self.__Curs__.execute("INSERT INTO Registro VALUES ('%s', '%s', '%s', %d , '%s', %d, %d, %d);"%(self.Zona,self.Region,self.Colonia,int(self.Secc),self.Nom,int(self.Work),int (self.Whats), int(self.Integ)))
-    self.__Con__.commit()
+      print("Introduzca la Zona: ")
+      self.Zona = input().upper()
       
+      print("Introduzca la Region: ")
+      self.Region = input().upper()
+
+      print("Introduzca la colonia del seccional: ")
+      self.Colonia = input().upper()
+
+    if Tables2:
+
+      self.__Curs__.execute("INSERT INTO Registro (Zona,Region,Colonia,Sección,Nombre,Labora,Grupo_Whats,Num_Integrantes,Num_Registros) VALUES ('%s', '%s', '%s', %d , '%s', %d, %d, %d, %d);"%(self.Zona,self.Region,self.Colonia,int(self.Secc),self.Nom,int(self.Work),int (self.Whats), int(self.Integ), int(self.Reg)))
+      self.__Con__.commit()
+    else:
+      self.__Curs__.execute("INSERT INTO Registro VALUES ('%d' , '%s', '%s', '%s', %d , '%s', %d, %d, %d, %d);"%(1,self.Zona,self.Region,self.Colonia,int(self.Secc),self.Nom,int(self.Work),int (self.Whats), int(self.Integ), int(self.Reg)))
+      self.__Con__.commit()
+
+
+  def disp_secc(self):
+
+    os.system("cls")
+
+    self.__Curs__.execute("Select id, Sección, Nombre from Registro")
+    secc = self.__Curs__.fetchall()
+
+    if secc:
+      print("Los seccionales registrados son: ")
+      for i in secc:
+        
+        print("\n%d\t%d\t%s"%i)
+
+      print("Digite el numero del seccional que quieres escojer (Para crear uno nuevo prescione 0): ")
+
       
-    
+      seccional = input()
+
+      if seccional == "0":
+        self.new_secc()
+        self.__search__()
+      else:
+        self.old_secc()
+        self.up_to_date(seccional)
+
+    else:
+
+      self.new_secc()
+      self.__search__()
+
--- main.py
+++ main.py
@@ -9,8 +9,11 @@ import Seccional as sec
 if __name__ == "__main__":
 
 	print("SISTEMA DE REGISTRO DE SECCIONALES\n\n")
+
 	print("Ingrese su nombre: ")
 	Nombre = input()
 
-	Secc = sec.seccional(Nombre) 
+	Secc = sec.seccional(Nombre)
+
+	Secc.disp_secc()
 		
